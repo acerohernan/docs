8f8eabb0cba738b1ae7ef1c3c4d8bad8e0fef7bd
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-03-11 21:31:03 -0500

    feat(server): hocuspocus server configured

--- apps/server/src/lib/hocuspocus.ts
+++ apps/server/src/lib/hocuspocus.ts
@@ -1,6 +1,30 @@
 import { Server } from "@hocuspocus/server";
 import { Logger } from "@hocuspocus/extension-logger";
 
+import { env } from "@/config/env.js";
+
+import { verifyAccessToken } from "@/services/auth.service.js";
+
+const isDev = env.NODE_ENV === "development";
+const isProd = env.NODE_ENV === "production";
+
 export const hocuspocus = Server.configure({
-  extensions: [new Logger()],
+  extensions: [
+    new Logger({
+      onConnect: isDev || isProd,
+      onChange: isDev,
+      onConfigure: isDev,
+      onDestroy: isDev,
+      onRequest: isDev,
+      onLoadDocument: isDev,
+      onStoreDocument: isDev,
+    }),
+  ],
+  onAuthenticate: async (payload) => {
+    if (!payload.token) throw new Error("unathorized");
+
+    const result = await verifyAccessToken(payload.token);
+
+    if (!result.success) throw new Error("unathorized");
+  },
 });
