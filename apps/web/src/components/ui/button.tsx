43e354d8e05c5b9dbb5e7b891f9f41d72a7fe765
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-03-11 21:38:46 -0500

    feat(web): add loading property to button

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
+import { Loader2Icon } from "lucide-react";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
@@ -35,12 +36,23 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, loading, disabled, children, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        disabled={loading || disabled}
+        ref={ref}
+        {...props}
+      >
+        {children}
+        {loading ? <Loader2Icon className={cn("h-4 w-4 animate-spin", children && "ml-2")} /> : null}
+      </Comp>
+    );
   },
 );
 Button.displayName = "Button";
