5f9389fac04b8b782296119998a8889a832f67d7
Hernan Acero @acerohernan <contacto.acero.hernan@gmail.com>
2024-03-13 22:50:50 -0500

    feat(server): hocuspocus database extension with typeorm implemented

--- apps/server/src/lib/hocuspocus.ts
+++ apps/server/src/lib/hocuspocus.ts
@@ -1,9 +1,14 @@
 import { Server } from "@hocuspocus/server";
 import { Logger } from "@hocuspocus/extension-logger";
+import { Database } from "@hocuspocus/extension-database";
 
 import { env } from "@/config/env.js";
+import { logger } from "@/config/logger.js";
 
 import { verifyAccessToken } from "@/services/auth.service.js";
+
+import { Document } from "@/typeorm/entities/document.js";
+import { DocumentRepository } from "@/typeorm/repositories/document.js";
 
 const isDev = env.NODE_ENV === "development";
 const isProd = env.NODE_ENV === "production";
@@ -19,8 +24,50 @@ export const hocuspocus = Server.configure({
       onLoadDocument: isDev,
       onStoreDocument: isDev,
     }),
+    new Database({
+      fetch: async ({ documentName: name }) => {
+        try {
+          let document: Document | null;
+
+          document = await DocumentRepository.findOneBy({
+            name,
+          });
+
+          if (!document) {
+            document = new Document();
+            document.name = name;
+            document.data = Buffer.alloc(0);
+            await DocumentRepository.save(document);
+            logger.debug("document created in db", { name });
+          }
+
+          return document.data;
+        } catch (error) {
+          logger.error("error at fetching document from database", error);
+          return null;
+        }
+      },
+      store: async (data) => {
+        try {
+          await DocumentRepository.update(
+            { name: data.documentName },
+            { data: data.state },
+          );
+          logger.debug("document successfully updated in DB", {
+            name: data.documentName,
+          });
+        } catch {
+          logger.error("error at updating document", {
+            name: data.documentName,
+          });
+        }
+      },
+    }),
   ],
   onAuthenticate: async (payload) => {
+    // allow fetch demo document without authentication
+    if (payload.documentName === "demo") return;
+
     if (!payload.token) throw new Error("unathorized");
 
     const result = await verifyAccessToken(payload.token);
